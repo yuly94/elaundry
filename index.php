f049d4304aedc690431ae8979d53f3f3e5a97565
yuly nurhidayati <yulynur2013@gmail.com>
2016-08-06 13:06:20 +0700

    Memperbaiki index, yg awalnya apabila terjadi error, muncul halaman error bertumpuk 2, sekarang sudah normal

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
 require 'config/paths.php';
 require 'config/database.php';
 require 'config/constants.php';
+
 require 'util/Auth.php';
 
-//Also spl_autoload_register
-function __autoload($class){
-	require LIBS.$class.'.php';
+// Also spl_autoload_register (Take a look at it if you like)
+function __autoload($class) {
+    require LIBS . $class .".php";
 }
+
+
 
  
 /* require 'libs/Bootstrap.php';
@@ -23,14 +26,14 @@ require 'libs/Database.php';
 require 'libs/Session.php';
 require 'libs/Hash.php';
  */
-
+ 
+// Load the Bootstrap!
 $bootstrap = new Bootstrap();
 
-		  
- // Optional Path Settings		
- //$bootstrap->setControllerPath();		
- //$bootstrap->setModelPath();		
- //$bootstrap->setDefaultFile();		
- //$bootstrap->setErrorFile();		
- 		
- $bootstrap->init();
+// Optional Path Settings
+//$bootstrap->setControllerPath();
+//$bootstrap->setModelPath();
+//$bootstrap->setDefaultFile();
+//$bootstrap->setErrorFile();
+
+$bootstrap->init();
