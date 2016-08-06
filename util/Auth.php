3b889cfb2bdad7ec3f962f5d1e9d9c6505c3f6cd
yuly nurhidayati <yulynur2013@gmail.com>
2016-08-06 18:12:32 +0700

    Memperbaiki url redirek nya

--- util/Auth.php
+++ util/Auth.php
@@ -7,17 +7,17 @@
  */
 class Auth
 {
+    
     public static function handleLogin()
     {
         @session_start();
-        $logged = Session::get('loggedIn');
-        if ($logged==false) {
-            Session::destroy();
-            header('location: ../login');
+        $logged = $_SESSION['loggedIn'];
+        if ($logged == false) {
+            session_destroy();
+            header('location:'.URL.'login');
             exit;
-		}
-        
+        }
     }
+    
 }
-
 
