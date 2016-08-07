920e350223853910d91e869c7ce9c93906a95ec6
yuly nurhidayati <yulynur2013@gmail.com>
2016-08-07 22:31:05 +0700

    Update View dengan 5 fungsi Header, HeaderCss, View, FooterJs, Footer

--- libs/View.php
+++ libs/View.php
@@ -10,8 +10,12 @@ class View{
 
     
 
-    public function render($name, $noInclude = 1)
-    {
+    public function render($viewName, 
+            $withHeader = true,
+            $withHeaderCss = true,
+            $withFooterJs = true, 
+            $withFooter = true )
+    { /*
         if ($noInclude == 1) {
             require 'views/' . $name . '.php';    
         }
@@ -23,6 +27,26 @@ class View{
             require 'views/header.php';
             require 'views/' . $name . '.php';
             require 'views/footer.php';    
+        }
+     
+     */
+        if ($withHeader == TRUE) {
+            require 'views/header.php';    
+        }
+        
+        if ($withHeaderCss == TRUE) {
+            require 'views/headerCss.php';      
+        }
+         if ($viewName != false) {
+            require 'views/' . $viewName . '.php';       
+        }
+        
+        if ($withFooterJs == TRUE) {
+            require 'views/footerJs.php';         
+        }
+        
+        if ($withFooter == TRUE) {
+            require 'views/footer.php';     
         }
     }
 
